235042f006ab679f24eccd394de3d0c555626d9c
jcderry5 <jcderry5@gmail.com>
2023-02-09 11:59:33 -0600

    Removed file that shouldn't be tracked

--- step-strikers-app/step-strikers-app/Armor.swift
+++ step-strikers-app/step-strikers-app/Armor.swift
@@ -5,4 +5,26 @@
 //  Created by Jalyn Derry on 2/9/23.
 //
 
-import Foundation
+enum RPGCharacterOrCaster {
+    case RPGCharacter(RPGCharacter)
+    case Caster(Caster)
+}
+
+protocol Armor {
+    var name: String { get set}
+    var wearers: <RPGCharacterOrCaster> {get set}
+    var armorClass: Int {get set}
+}
+
+struct leather: Armor {
+    var armorClass: Int
+    var wearers: <RPGCharacterOrCaster> {get set}
+    
+    func init(){
+        armorClass = 11
+        wearers = {Rogue, Bard}
+        
+    }
+}
+
+
