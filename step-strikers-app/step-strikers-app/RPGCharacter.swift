d4ce60b33b41145b10939808533ebf3503391b15
Jalyn Derry <jcderry5@gmail.com>
2023-02-09 11:02:27 -0600

    Create protocols (#3)
    * Old files from when i branched
    
    * Succeeds except for firebase. Requires GoogleService-Info.plist

--- step-strikers-app/step-strikers-app/RPGCharacter.swift
+++ step-strikers-app/step-strikers-app/RPGCharacter.swift
@@ -5,11 +5,47 @@
 //  Created by Jalyn Derry on 2/7/23.
 //
 
+<<<<<<< HEAD
 
 import Foundation
 
 
 class RPGCharacter {
+=======
+
+import Foundation
+
+
+protocol RPGCharacter: CustomStringConvertible {
+    var characterName: String { get set}
+    var userName: String { get set}
+    var maxHealth: Int { get set}
+    var currHealth: Int { get set}
+    var maxStamina: Int { get set}
+    var currStamina: Int { get set}
+    
+    init(characterName: String, userName: String, health: Int,
+            stamina: Int)
+}
+
+
+protocol Caster {
+    var characterName: String { get set}
+    var userName: String { get set}
+    var maxHealth: Int { get set}
+    var currHealth: Int { get set}
+    var maxStamina: Int { get set}
+    var currStamina: Int { get set}
+    var currSpellPoints: Int { get set}
+    var maxSpellPoints: Int { get set}
+    
+    init(characterName: String, userName: String, health: Int,
+                stamina: Int, spellPoints: Int)
+}
+
+
+class Fighter: RPGCharacter {
+>>>>>>> Create protocols (#3)
     var characterName: String
     var userName: String
     
@@ -38,6 +74,7 @@ class RPGCharacter {
         self.currHealth = health
         self.maxStamina = stamina
         self.currStamina = stamina
+<<<<<<< HEAD
         
         self.weaponsInInventory = [fistsWeapon]
         self.currWeapon = fistsWeapon
@@ -124,6 +161,95 @@ class Caster: RPGCharacter {
         self.currSpellPoints -= amtDecrease
         
         // TODO: Do something when out of spell points
+=======
+    }
+}
+
+
+class Wizard: Caster {
+    
+    var characterName: String
+    var userName: String
+    
+    var maxHealth: Int
+    var currHealth: Int
+    
+    var maxStamina: Int
+    var currStamina: Int
+    
+    var currSpellPoints: Int
+    var maxSpellPoints: Int
+    
+    var description: String {
+        return ("Character Name: \(characterName)\nUser Name: \(userName)\nCurrent Health: \(currHealth)\nCurrent Stamina: \(currStamina)\nCurrent Spellpoints: \(currSpellPoints)")
+    }
+    
+    required init(characterName: String, userName: String, health: Int, stamina: Int, spellPoints: Int) {
+        self.characterName = characterName
+        self.userName = userName
+        self.maxHealth = health
+        self.currHealth = health
+        self.maxStamina = stamina
+        self.currStamina = stamina
+        self.maxSpellPoints = spellPoints
+        self.currSpellPoints = spellPoints
+    }
+    
+}
+
+
+class Bard: Caster {
+    var characterName: String
+    var userName: String
+    
+    var maxHealth: Int
+    var currHealth: Int
+    
+    var maxStamina: Int
+    var currStamina: Int
+    
+    var currSpellPoints: Int
+    var maxSpellPoints: Int
+    
+    var description: String {
+        return ("Character Name: \(characterName)\nUser Name: \(userName)\nCurrent Health: \(currHealth)\nCurrent Stamina: \(currStamina)\nCurrent Spellpoints: \(currSpellPoints)")
+    }
+    
+    required init(characterName: String, userName: String, health: Int, stamina: Int, spellPoints: Int) {
+        self.characterName = characterName
+        self.userName = userName
+        self.maxHealth = health
+        self.currHealth = health
+        self.maxStamina = stamina
+        self.currStamina = stamina
+        self.maxSpellPoints = spellPoints
+        self.currSpellPoints = spellPoints
+    }
+}
+
+
+class Rogue: RPGCharacter {
+    var characterName: String
+    var userName: String
+    
+    var maxHealth: Int
+    var currHealth: Int
+    
+    var maxStamina: Int
+    var currStamina: Int
+    
+    required init(characterName: String, userName: String, health: Int, stamina: Int) {
+        self.characterName = characterName
+        self.userName = userName
+        self.maxHealth = health
+        self.currHealth = health
+        self.maxStamina = stamina
+        self.currStamina = stamina
+    }
+    
+    var description: String {
+        return ("Character Name: \(characterName)\nUser Name: \(userName)\nCurrent Health: \(currHealth)\nCurrent Stamina: \(currStamina)")
+>>>>>>> Create protocols (#3)
     }
 }
 
@@ -141,6 +267,7 @@ class Fighter: RPGCharacter {
     }
 }
 
+<<<<<<< HEAD
 
 class Wizard: Caster {
     var description: String {
@@ -174,3 +301,5 @@ class Rogue: RPGCharacter {
     
 }
 
+=======
+>>>>>>> Create protocols (#3)
