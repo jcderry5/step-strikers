d9560d59a09c4ec7761ca425ae5c363853ae52c6
jcderry5 <jcderry5@gmail.com>
2023-02-09 12:08:33 -0600

    Added gitignore

--- step-strikers-app/commandline-testing.playground/Contents.swift
+++ step-strikers-app/commandline-testing.playground/Contents.swift
@@ -1,9 +1,1 @@
-print("helloWorld")
 
-print("Starting!")
-var player1 = Fighter(characterName: "Nick", userName: "niceNick", health: 30, stamina: 45)
-var player2 = Fighter(characterName: "Jalyn", userName: "jubilantJalyn", health: 30, stamina: 45)
-print("Made the 2 fighters")
-print(player1)
-print(player2)
-print(player1.characterName)
