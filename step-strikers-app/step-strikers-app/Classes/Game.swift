e79ff241de1c27a53dd7b794693708e2006b523b
Jalyn Derry <jcderry5@gmail.com>
2023-02-22 14:56:17 -0600

    Adding classes (#16)
    * Adding Classes directory and empty class files
    
    * Random suspicious files

---
+++ step-strikers-app/step-strikers-app/Classes/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  step-strikers-app
+//
+//  Created by Jalyn Derry on 2/22/23.
+//
+
+import Foundation
---
+++ step-strikers-app/step-strikers-app/Classes/RPGCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RPGCharacter.swift
+//  step-strikers-app
+//
+//  Created by Jalyn Derry on 2/22/23.
+//
+
+import Foundation
