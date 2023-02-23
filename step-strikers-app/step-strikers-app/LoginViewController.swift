57fa1a7ce278584d661a6dded92bc7eff308eb28
Nicholas Huang <nhua5610@gmail.com>
2023-02-23 16:33:48 -0600

    Initial commit

---
+++ step-strikers-app/step-strikers-app/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  step-strikers-app
+//
+//  Created by Nicholas Huang on 2/23/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ step-strikers-app/step-strikers-app/RegistrationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationViewController.swift
+//  step-strikers-app
+//
+//  Created by Nicholas Huang on 2/23/23.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
