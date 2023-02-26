50d93f929051f2a84290c1d75c27373bd769f742
Nicholas Huang <nhua5610@gmail.com>
2023-02-26 05:38:38 -0600

    added stats display and characters

--- step-strikers-app/step-strikers-app/Battle Screens/BattleRollViewController.swift
+++ step-strikers-app/step-strikers-app/Battle Screens/BattleRollViewController.swift
@@ -7,23 +7,100 @@
 
 import UIKit
 
-class BattleRollViewController: UIViewController {
+class BattleRollViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var actions: [Action] = [Action]()
+    let cellId = "actionCell"
+    let statsCellID = "statsCell"
+    let statsHeaderID = "statsHeader"
+    var stats: [StatsRow] = [StatsRow]()
+    var header: [StatsHeaderRow] = [StatsHeaderRow]()
+    var actionDisplay:UITableView = UITableView()
+    var statsDisplay:UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // puts full screen image as background of view controller
+        // sets up the background images of the view controller
+        // THESE NEED TO HAPPEN IN ORDER!!!!
+        assignBackground()
+        createBattleActionMenu()
+        createBattleStatsDisplay()
+        createSettingsButton(x: 10, y: 50, width: 40, height: 40)
+        
+        // create characters
+        // will need to change "name" based on what the enemy players are
+        // TODO: update to take what the enemies character type are
+        drawEnemies(enemy1: "Fighter", enemy2: "Bard", enemy3: "Rogue", enemy4: "Wizard")
+        
+        // stats menu
+        createStatsArray()
+        statsDisplay = UITableView(frame: CGRect(x: self.view.safeAreaInsets.left+40, y: 140, width: 300, height: 300))
+        statsDisplay.translatesAutoresizingMaskIntoConstraints = false
+        statsDisplay.dataSource = self
+        // register the table since it was not created with the storyboard
+        // two different types of custom cells hence
+        // need to register both types
+        statsDisplay.register(StatsTableViewCell.self, forCellReuseIdentifier: statsCellID)
+        statsDisplay.register(StatsHeaderTableViewCell.self, forCellReuseIdentifier: statsHeaderID)
+        statsDisplay.backgroundColor = UIColor.clear
+        statsDisplay.delegate = self
+        // cannot press on a row
+        statsDisplay.allowsSelection = false
+        // get rid of grey separator line in between rows
+        statsDisplay.separatorColor = UIColor.clear
+        self.view.addSubview(statsDisplay)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stats.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // top stats display
+        if tableView == statsDisplay {
+            if indexPath.row == 0 {
+                // if its the first row then it just should be the header
+                // of team member names
+                let cell = tableView.dequeueReusableCell(withIdentifier: statsHeaderID, for: indexPath) as! StatsHeaderTableViewCell
+                let currentLastAction = header[indexPath.row]
+                cell.head = currentLastAction
+                // set background of each cell to clear
+                cell.backgroundColor = UIColor.clear
+                // no separator inset!
+                cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: statsCellID, for: indexPath) as! StatsTableViewCell
+                let currentLastAction = stats[indexPath.row]
+                cell.stats = currentLastAction
+                cell.backgroundColor = UIColor.clear
+                cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+                return cell
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath:IndexPath) -> CGFloat {
+        if tableView == statsDisplay {
+            return 30
+        } else {
+            if indexPath.row == 0 {
+                return 80
+            }
+        }
+        
+        return UITableView.automaticDimension
+    }
+    
+    func createStatsArray() {
+        header.append(StatsHeaderRow(names: ["Host", "Player 1", "Player 2", "Player 3"]))
+        // extra to account for header messing everything up
+        stats.append(StatsRow(imageName: UIImage(named: "health"), points: [1,2,3,4] , totalPoints: [1,2,3,4]))
+        stats.append(StatsRow(imageName: UIImage(named: "health"), points: [1,2,3,4] , totalPoints: [1,2,3,4]))
+        stats.append(StatsRow(imageName: UIImage(named: "SpellPoints"), points: [1,2,3,4] , totalPoints: [1,2,3,4]))
+        stats.append(StatsRow(imageName: UIImage(named: "lightningbolt"), points:[1,2,3,4] , totalPoints: [1,2,3,4]))
+    }
 
 }
