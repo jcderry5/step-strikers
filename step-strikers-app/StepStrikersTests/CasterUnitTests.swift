94d90bf5d266e985ef5e71515b5098647cefc652
jcderry5 <jcderry5@gmail.com>
2023-02-28 23:56:39 -0600

    Create a new Caster.swift, because mine was not appearing locally

--- step-strikers-app/StepStrikersTests/CasterUnitTests.swift
+++ step-strikers-app/StepStrikersTests/CasterUnitTests.swift
@@ -6,38 +6,15 @@
 //
 
 import XCTest
-@testable import step_strikers_app
 
 final class CasterUnitTests: XCTestCase {
 
-    var fistsForPlayerOne: Weapon!
-    var fistsForPlayerTwo: Weapon!
-    var noArmorForPlayerOne: Armor!
-    var noArmorForPlayerTwo: Armor!
-    var playerOne: Fighter!
-    var playerTwo: Wizard!
-    
     override func setUpWithError() throws {
-        fistsForPlayerOne = fists()
-        fistsForPlayerTwo = fists()
-
-        noArmorForPlayerOne = noArmor()
-        noArmorForPlayerTwo = noArmor()
-        
-        playerOne = Fighter(characterName: "Roywyn", userName: "jazzyLinkage", health: 30, stamina: 30, currWeapon: fistsForPlayerOne, weaponsInInventory: [fistsForPlayerOne], currArmor: noArmorForPlayerOne, armorInInventory: [noArmorForPlayerOne], itemsInInventory: [])
-
-        playerTwo = Wizard(characterName: "Althea", userName: "kellyTheKicker", health: 30, stamina: 30, spellPoints: 30, currWeapon: fistsForPlayerTwo, weaponsInInventory: [fistsForPlayerTwo], currArmor: noArmorForPlayerTwo, armorInInventory: [noArmorForPlayerTwo], itemsInInventory: [])
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        fistsForPlayerOne = nil
-        fistsForPlayerTwo = nil
-
-        noArmorForPlayerOne = nil
-        noArmorForPlayerTwo = nil
-        
-        playerOne = nil
-        playerTwo = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
@@ -55,23 +32,4 @@ final class CasterUnitTests: XCTestCase {
         }
     }
 
-    func testSplitObjAndUseCount() {
-        // Arrange
-        let conjoinedString1: String = "01Fists"
-        let conjoinedString2: String = "14Battle Axe"
-        let conjoinedString3: String = "00Long Sword"
-        
-        // Act
-        let tuple1 = splitObjAndUseCount(objWithUseCount: conjoinedString1)
-        let tuple2 = splitObjAndUseCount(objWithUseCount: conjoinedString2)
-        let tuple3 = splitObjAndUseCount(objWithUseCount: conjoinedString3)
-        
-        // Assert
-        XCTAssertTrue(tuple1.useCount == 1)
-        XCTAssertTrue(tuple1.objectName == "Fists")
-        XCTAssertTrue(tuple2.useCount == 14)
-        XCTAssertTrue(tuple2.objectName == "Battle Axe")
-        XCTAssertTrue(tuple3.useCount == 0)
-        XCTAssertTrue(tuple3.objectName == "Long Sword")
-    }
 }
