193b69b26b602190c2347c5b52c651b8d204da73
Nicholas Huang <nhua5610@gmail.com>
2023-03-01 12:36:08 -0600

    removed textfield delegate

--- step-strikers-app/step-strikers-app/GeneralUIViewControllerExtensions.swift
+++ step-strikers-app/step-strikers-app/GeneralUIViewControllerExtensions.swift
@@ -8,7 +8,7 @@
 import UIKit
 import FirebaseFirestore
 
-extension UIViewController: UITextFieldDelegate {
+extension UIViewController {
     
     func assignBackground() {
         let background = UIImage(named: "Background")
@@ -35,7 +35,6 @@ extension UIViewController: UITextFieldDelegate {
         field.borderStyle = UITextField.BorderStyle.roundedRect
         field.isSecureTextEntry = secured
         field.autocapitalizationType = .none
-        field.delegate = self
         view.addSubview(field)
         return field
     }
