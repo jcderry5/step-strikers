ee5bb2d936c0b6b5544c1d924e343dc478ba7746
Nicholas Huang <nhua5610@gmail.com>
2023-03-03 06:33:51 -0600

    populated display board

--- step-strikers-app/step-strikers-app/StatsViewController.swift
+++ step-strikers-app/step-strikers-app/StatsViewController.swift
@@ -25,6 +25,24 @@ class StatsViewController: UIViewController {
         // Create the display board and its contents
         _ = createImage(x:0, y:219, w:393, h:374, name:"Display Board")
         
+        // TODO: Pull stats data from Firebase and use as label text
+        _ = createImage(x: 25, y: 251, w: 75, h: 75, name: "health")
+        _ = createLabel(x: 116, y: 251, w: 150, h: 65, font: "munro", size: 33, text: "__/__", align: .left)
+        _ = createImage(x: 25, y: 326, w: 70, h: 70, name: "SpellPoints")
+        _ = createLabel(x: 116, y: 326, w: 150, h: 65, font: "munro", size: 33, text: "__/__", align: .left)
+        _ = createImage(x: 45, y: 411, w: 45, h: 65, name: "lightningbolt")
+        _ = createLabel(x: 116, y: 411, w: 150, h: 65, font: "munro", size: 33, text: "__/__", align: .left)
+        _ = createImage(x: 35, y: 486, w: 65, h: 75, name: "Shield")
+        _ = createLabel(x: 116, y: 496, w: 150, h: 65, font: "munro", size: 33, text: "__", align: .left)
+
+        // TODO: Display time data in the case that stat values are not at max
+        _ = createImage(x: 227, y: 261, w: 35, h: 45, name: "Hourglass")
+        _ = createLabel(x: 265, y: 251, w: 150, h: 75, font: "munro", size: 33, text: "__ min", align: .left)
+        _ = createImage(x: 227, y: 336, w: 35, h: 45, name: "Hourglass")
+        _ = createLabel(x: 265, y: 326, w: 150, h: 75, font: "munro", size: 33, text: "__ min", align: .left)
+        _ = createImage(x: 227, y: 421, w: 35, h: 45, name: "Hourglass")
+        _ = createLabel(x: 265, y: 411, w: 150, h: 75, font: "munro", size: 33, text: "__ min", align: .left)
+        
         // Display steps info
         _ = createImage(x: 25, y: 618, w: 75, h: 75, name: "brown boots")
         // TODO: Pull steps info from firebase and display it here
