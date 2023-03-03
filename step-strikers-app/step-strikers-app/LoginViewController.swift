c7bf53e4e40ef8710b38c6acfaf081287c56ea37
Nicholas Huang <55514223+OddJerbb@users.noreply.github.com>
2023-03-02 18:01:06 -0600

    Now returning objects in extension file (#46)
    * now returning objects in extension file
    
    * added image parameters to createButton

--- step-strikers-app/step-strikers-app/LoginViewController.swift
+++ step-strikers-app/step-strikers-app/LoginViewController.swift
@@ -13,31 +13,34 @@ class LoginViewController: UIViewController {
     let munro = "munro"
     var usernameTextField:UITextField?
     var passwordTextField:UITextField?
+    
+    let buttonImg = UIImage(named:"Menu Button")
+    let selectedImg = UIImage(named:"Selected Menu Button")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         assignBackground()
         
         // Create title image
-        createImage(x:6, y:75, w:380, h:200, name:"Title")
+        _ = createImage(x:6, y:75, w:380, h:200, name:"Title")
         
         // Username
-        createLabel(x:9, y:333, w:137, h:25, font:munro, size:22, text:"Username:")
+        _ = createLabel(x:9, y:333, w:137, h:25, font:munro, size:22, text:"Username:", align:.right)
         let username = createTextField(x:154, y:333, w:202, h:34, secured:false)
         self.usernameTextField = username!
         username?.text = ""
         // Password
-        createLabel(x:9, y:403, w:137, h:25, font:munro, size:22, text:"Password:")
+        _ = createLabel(x:9, y:403, w:137, h:25, font:munro, size:22, text:"Password:", align:.right)
         let password = createTextField(x:154, y:403, w:202, h:34, secured:true)
         self.passwordTextField = password!
         password?.text = ""
         
         // Register button design
-        let loginButton = createButton(x:116, y:560, w:160, h:100, text:"LOG IN", fontSize:24)
+        let loginButton = createButton(x:116, y:560, w:160, h:100, text:"LOG IN", fontSize:24, normalImage:buttonImg!, highlightedImage:selectedImg!)
         loginButton.addTarget(self, action:#selector(loginPressed), for:.touchUpInside)
         
         // Signin button design
-        let signupButton = createButton(x:116, y:668, w:160, h:100, text:"SIGN UP", fontSize:24)
+        let signupButton = createButton(x:116, y:668, w:160, h:100, text:"SIGN UP", fontSize:24, normalImage:buttonImg!, highlightedImage:selectedImg!)
         signupButton.addTarget(self, action:#selector(signupPressed), for:.touchUpInside)
     }
 
--- step-strikers-app/step-strikers-app/RegistrationViewController.swift
+++ step-strikers-app/step-strikers-app/RegistrationViewController.swift
@@ -12,36 +12,39 @@ class RegistrationViewController: UIViewController {
     let munro = "munro"
     var usernameTextField:UITextField?
     var passwordTextField:UITextField?
+    
+    let buttonImg = UIImage(named:"Menu Button")
+    let selectedImg = UIImage(named:"Selected Menu Button")
         
     override func viewDidLoad() {
         super.viewDidLoad()
         assignBackground()
         
         // Create title image
-        createImage(x:6, y:75, w:380, h:200, name:"Title")
+        _ = createImage(x:6, y:75, w:380, h:200, name:"Title")
         
         // Username
-        createLabel(x:9, y:321, w:137, h:25, font:munro, size:22, text:"Username:")
+        _ = createLabel(x:9, y:321, w:137, h:25, font:munro, size:22, text:"Username:", align:.right)
         var username = createTextField(x:154, y:321, w:202, h:34, secured:false)
         self.usernameTextField = username!
         username?.text = ""
         
         // Password
-        createLabel(x:9, y:372, w:137, h:25, font:munro, size:22, text:"Password:")
+        _ = createLabel(x:9, y:372, w:137, h:25, font:munro, size:22, text:"Password:", align:.right)
         var password = createTextField(x:154, y:372, w:202, h:34, secured:true)
         self.passwordTextField = password!
         password?.text = ""
         
         // Confirm password
-        createLabel(x:9, y:423, w:137, h:25, font:munro, size:22, text:"Confirm Pwd:")
-        createTextField(x:154, y:423, w:202, h:34, secured:true)
+        _ = createLabel(x:9, y:423, w:137, h:25, font:munro, size:22, text:"Confirm Pwd:", align:.right)
+        _ = createTextField(x:154, y:423, w:202, h:34, secured:true)
                 
         // Register button design
-        let registerButton = createButton(x:116, y:560, w:160, h:100, text:"REGISTER", fontSize:24)
+        let registerButton = createButton(x:116, y:560, w:160, h:100, text:"REGISTER", fontSize:24, normalImage:buttonImg!, highlightedImage:selectedImg!)
         registerButton.addTarget(self, action:#selector(registerPressed), for:.touchUpInside)
         
         // Signin button design
-        let signInButton = createButton(x:116, y:668, w:160, h:100, text:"SIGN IN", fontSize:24)
+        let signInButton = createButton(x:116, y:668, w:160, h:100, text:"SIGN IN", fontSize:24, normalImage:buttonImg!, highlightedImage:selectedImg!)
         signInButton.addTarget(self, action:#selector(signInPressed), for:.touchUpInside)
     }
     
