da5a956c01681672256c297803e7f2210fa1419c
Nicholas Huang <nhua5610@gmail.com>
2023-03-03 13:31:11 -0600

    initial commit

---
+++ step-strikers-app/step-strikers-app/InventoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InventoryViewController.swift
+//  step-strikers-app
+//
+//  Created by Nicholas Huang on 3/3/23.
+//
+
+import UIKit
+
+class InventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
