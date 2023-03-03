00197f5e47986356a99f8475f40699cdcb86a327
Nicholas Huang <nhua5610@gmail.com>
2023-03-02 18:15:19 -0600

    initial commit

---
+++ step-strikers-app/step-strikers-app/BattleMenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BattleMenuViewController.swift
+//  step-strikers-app
+//
+//  Created by Nicholas Huang on 3/2/23.
+//
+
+import UIKit
+
+class BattleMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ step-strikers-app/step-strikers-app/StatsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StatsViewController.swift
+//  step-strikers-app
+//
+//  Created by Nicholas Huang on 3/2/23.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
