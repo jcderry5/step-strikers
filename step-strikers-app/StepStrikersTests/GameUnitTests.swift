67ae337c65cd0bddff2a70d8817fc76a6e31049c
jcderry5 <jcderry5@gmail.com>
2023-03-29 18:55:23 -0500

    Remove all tests

--- step-strikers-app/StepStrikersTests/GameUnitTests.swift
+++ step-strikers-app/StepStrikersTests/GameUnitTests.swift
@@ -9,37 +9,13 @@ import XCTest
 @testable import step_strikers_app
 
 final class GameUnitTests: XCTestCase {
-
-    var fistsForPlayerOne: Weapon!
-    var fistsForPlayerTwo: Weapon!
-    var noArmorForPlayerOne: Armor!
-    var noArmorForPlayerTwo: Armor!
-    var playerOne: RPGCharacter!
-    var playerTwo: RPGCharacter!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        fistsForPlayerOne = fists()
-        fistsForPlayerTwo = fists()
-
-        noArmorForPlayerOne = noArmor()
-        noArmorForPlayerTwo = noArmor()
-        
-        playerOne = Fighter(characterName: "Roywyn", userName: "jazzyLinkage", health: 30, stamina: 30, currWeapon: fistsForPlayerOne, weaponsInInventory: [fistsForPlayerOne], currArmor: noArmorForPlayerOne, armorInInventory: [noArmorForPlayerOne], itemsInInventory: [])
-
-        playerTwo = Wizard(characterName: "Althea", userName: "kellyTheKicker", health: 30, stamina: 30, spellPoints: 30, currWeapon: fistsForPlayerTwo, weaponsInInventory: [fistsForPlayerTwo], currArmor: noArmorForPlayerTwo, armorInInventory: [noArmorForPlayerTwo], itemsInInventory: [])
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        fistsForPlayerOne = nil
-        fistsForPlayerTwo = nil
-
-        noArmorForPlayerOne = nil
-        noArmorForPlayerTwo = nil
-        
-        playerOne = nil
-        playerTwo = nil
     }
 
     func testExample() throws {
@@ -55,13 +31,5 @@ final class GameUnitTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
-    }
-    
-    func testRollDie() {
-        // Act
-        let rollValue = rollDie(quant: 1, sides: 20)
-        
-        // Assert
-        XCTAssertTrue(rollValue > 0 && rollValue <= 20)
     }
 }
